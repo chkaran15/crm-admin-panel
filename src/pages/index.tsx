8083e25bc22e54cd10d977704932ddc042076171
Karan Chaudhary <tharukaran930@gmail.com>
2025-06-10 17:26:04 +0545

    setup husky commitlinter lintstaged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       <h1 className="text-lg font-bold">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} afasd
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
